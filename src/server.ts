cc2027f19f62c343ac7d901a8bc4d8c3e712c573
alzamo12 <rafiqulislam4969@gmail.com>
2026-07-01 13:52:43 +0600

    fix tsconfig file

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import app from "./app";
-import config from "./config/index";
-import { initDB } from "./db/index";
+import config from "./config";
+import { initDB } from "./db";
 
 const main = () => {
     initDB()
-    // app.listen(config.port, () => {
-    //     console.log(`dev pulse is running on port: ${config.port}`)
-    // });
+    app.listen(config.port, () => {
+        console.log(`dev pulse is running on port: ${config.port}`)
+    });
 };
 
 main();
